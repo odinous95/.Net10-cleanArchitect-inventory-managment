abc3f15f73987ddc6f352b572d6d2632fa00598c
Odin Thif <mjd.thif@icloud.com>
2025-10-26 20:45:54 +0100

    Add InventoryRepository implementation with GetInventorieByNameAsync method

---
+++ ims.Plugins/ims.Plugins.inMomery/InventoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using ims.CoreEntities;
+using ims.UseCases.PluginsInterfaces;
+
+namespace ims.Plugins.inMomery
+{
+    public class InventoryRepository : IInventoryRepository
+    {
+        public Task<IEnumerable<Inventory>> GetInventorieByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
